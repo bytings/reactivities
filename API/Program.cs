8fe54bd5866bb3eb6b2f8b3fb7ccc1cacdb890cf
Daniel Mendoza <el_chino07@hotmail.com>
2020-11-20 07:55:25 -0300

    initial commit

---
+++ API/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Host = CreateHostBuilder(args).Build();
+
+            using(var scope = Host.Services.CreateScope())
+            {
+                var Services = scope.ServiceProvider;
+                try
+                {
+                    var context = Services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = Services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"An error occured during migration");
+                }
+            }
+
+            Host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
